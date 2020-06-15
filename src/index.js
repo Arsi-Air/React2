4e4cb1ed57ad0090eace621e35f65067caa1ae8f
Arsi-Air <32060641+Arsi-Air@users.noreply.github.com>
2020-06-15 23:06:13 +0400

    Completed

--- src/index.js
+++ src/index.js
@@ -1,9 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "./index.css";
 
 function Room() {
+  const [isLit, setLit] = React.useState(false);
+  const [isTemp, setTemp] = React.useState(22);
+  const brightness = isLit ? "lit" : "dark";
+  
   return (
-    <div className="room">The room is LiT</div>
+    <div className={`room ${brightness}`}>
+    The room is {isLit ? 'lit' : 'dark'}
+    <br />
+    The temperature is {isTemp}°C
+    <br />
+    <button onClick={() => setTemp(isTemp + 1)}>
+      + temp
+    </button>
+    <button onClick={() => setTemp(isTemp - 1)}>
+      + temp
+    </button>
+    <br />
+    <button onClick={() => setLit(!isLit)}>
+      flip the switch
+    </button>
+    <br />
+    <button onClick={() => setLit(true)}>
+      Turn lights on
+    </button>
+    <br />
+    <button onClick={() => setLit(false)}>
+      Turn lights off
+    </button>
+    </div>
   );
 }
 
